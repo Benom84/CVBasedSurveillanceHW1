43e329f87cf710ccc58957a47183d08c0f944627
Nir Barzilay <nbtk123@gmail.com>
2015-11-28 23:47:11 +0200

    renamed a file

--- plotCompareToGT.m
+++ plotCompareToGT.m
@@ -1,23 +1,20 @@
-function [FalsePositives,FalseNegatives] = plotCompareToGT(actualBinMatrix, expectedBinMatrix)
-%The function runs through all the frames, and plots on a graph the
-%falsePositives and falseNegatives, as function of time (= frames)
+function [fprs, fnrs] = plotNaiveGT(resultVideo, gtVideo)
+%Given a binary video and its grount-truth, will plot a graph of FPRs and
+%FNRs as function of the frames progress.
 
-matSize = size(actualBinMatrix);
-framesNum = matSize(4);
 
-comparisonData = zeros(framesNum,6);
+framesNum = size(resultVideo,3);
 
-for i = 1:framesNum
-    actual = actualBinMatrix(:,:,i);
-    expected = expectedBinMatrix(:,:,i);
-    
-    %[truePosRate, trueNegRate, Precision, NPV, FPR, FNR]
-    comparisonData(i) = compareResults2GroundTruth(actual, expected);
+fprs = zeros(framesNum);
+fnrs = zeros(framesNum);
+
+for i=1 : framesNum
+    [fpr, fnr] = compareResults2GroundTruth(resultVideo(:,:,i), gtVideo(:,:,i));
+    fprs(i) = fpr;
+    fnrs(i) = fnr;
 end
 
-FalsePositives = comparisonData(:,5);
-FalseNegatives = comparisonData(:,6);
-plot(1:framesNum, FalsePositives);
+plot(1:framesNum, fprs, 1:framesNum, fnrs);
 
 end
 
