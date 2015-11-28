09476abc2ad3b4a2b02239ea7bf4a39f27eab2e4
Nir Barzilay <nbtk123@gmail.com>
2015-11-28 20:27:06 +0200

    Extending the theoretical questions & renaming a file

---
+++ plotGT.m
@@ -0,0 +1,20 @@
+function [fprs, fnrs] = plotNaiveGT(resultVideo, gtVideo)
+%Given a binary video and its grount-truth, will plot a graph of FPRs and
+%FNRs as function of the frames progress.
+
+
+framesNum = size(resultVideo,3);
+
+fprs = zeros(framesNum);
+fnrs = zeros(framesNum);
+
+for i=1 : framesNum
+    [fpr, fnr] = compareResults2GroundTruth(resultVideo(:,:,i), gtVideo(:,:,i));
+    fprs(i) = fpr;
+    fnrs(i) = fnr;
+end
+
+plot(1:framesNum, fprs, 1:framesNum, fnrs);
+
+end
+
