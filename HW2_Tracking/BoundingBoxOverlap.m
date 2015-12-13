d15532138c54c82ad1d0847d344c813b0be84164
Benom84 <menahem.ben@gmail.com>
2015-12-13 18:26:21 +0200

    Added colored boxes for different boxes

---
+++ HW2_Tracking/BoundingBoxOverlap.m
@@ -0,0 +1,19 @@
+function result = BoundingBoxOverlap(BBox1, BBox2)
+
+    BBox1XMin = BBox1(1);
+    BBox1XMax = BBox1XMin + BBox1(3);
+    BBox1YMin = BBox1(2);
+    BBox1YMax = BBox1YMin + BBox1(4);
+    
+    BBox2XMin = BBox2(1);
+    BBox2XMax = BBox2XMin + BBox2(3);
+    BBox2YMin = BBox2(2);
+    BBox2YMax = BBox2YMin + BBox2(4);
+
+    result = 0;
+    if ~((BBox1XMax < BBox2XMin) || (BBox1XMin > BBox2XMax))
+        if ~((BBox1YMax < BBox2YMin) || (BBox1YMin > BBox2YMax))
+            result = 1;
+        end
+    end
+end
--- HW2_Tracking/alg_exe_script.m
+++ HW2_Tracking/alg_exe_script.m
@@ -1,4 +1,4 @@
 reader = VideoReader('./data/PETS2014-0101-3.avi');
 origVideo = reader.read();
-nbs_clean = track_nbs(origVideo);
+nbs_clean = track_nbs(origVideo, 400);
 implay(nbs_clean);
