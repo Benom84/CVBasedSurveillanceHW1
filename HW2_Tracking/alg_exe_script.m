68703e3a479b91c02551ad750eeffcfee267cbe5
Nir Barzilay <nbtk123@gmail.com>
2015-12-16 19:00:25 +0200

    Started the DOCX for HW2, and documented the code

--- HW2_Tracking/alg_exe_script.m
+++ HW2_Tracking/alg_exe_script.m
@@ -1,4 +1,9 @@
+fprintf('reading video...\n');
 reader = VideoReader('./data/PETS2014-0101-3.avi');
 origVideo = reader.read();
-nbs_clean = track_nbs(origVideo, 400);
+
+fprintf('running the tracking algorithm...\n');
+nbs_clean = track_nbs(origVideo, 500);
+
+fprintf('done tracking_nbs!');
 implay(nbs_clean);
